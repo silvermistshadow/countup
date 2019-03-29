40230982477f28474c95b4168e28c797a9751f12
silvermistshadow <bmead72@gmail.com>
2019-03-29 11:55:25 -0700

    start writing javascript

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+//Backend logic
+
+var countUp = function(countTo, countBy) {
+  if ((countTo > countBy) && (countBy > 0)) {
+    for (var counting = 0; counting <= countTo; counting += countBy) {
+      $("#output").append(counting + " ");
+    }
+  }
+  else if ((countTo < countBy) && (countTo > 0)) {
+    $("#outputErrorOne").show();
+  }
+  else if ((countTo < 0) || (countBy < 0)) {
+    $("#outputErrorNeg").show();
+  }
+  else if ((countTo === 0) || (countBy === 0)) {
+    $("#outputErrorZero").show();
+  }
+  else if ((Number.isNaN(countTo)) || (Number.isNaN(countBy))) {
+    $("#outputErrorNaN").show();
+  }
+  else if ()
+
+};
+
+
+
+
+
+
+
+
+
+//User Interface begins here
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    var countTo = parseInt($("#countTo").val());
+    var countBy = parseInt($("#countBy").val());
+
+
+  })
+
+
+
+});
